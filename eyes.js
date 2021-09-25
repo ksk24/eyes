68885d6e68a1a68a548e0b9004de411678e605da
Austin B <89538206+ksk24@users.noreply.github.com>
2021-09-25 18:25:42 -0400

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,11 +1,14 @@
-const balls = document.getElementsByClassName('ball');
+let balls = document.getElementsByClassName('ball');
 document.onmousemove = (event) => {
-  const x = (event.clientX * 100) / window.innerWidth + '%';
-  const y = (event.clientY * 100) / window.innerHeight + '%';
+  let x = (event.clientX * 100) / window.innerWidth + '%';
+  let y = (event.clientY * 100) / window.innerHeight + '%';
+
+
 
   for (let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
     balls[i].transform = 'translate(-' + x + ',-' + y + ')';
   }
+
 };
